f700bc24f17fa33f13b43ea875e83d1c5a138201
MP93 <80913365+marcoo933@users.noreply.github.com>
2023-06-09 20:43:54 +0200

    created api product from id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,68 +1,106 @@
-// Importa il modulo 'express'
 const express = require('express');
-
-// Crea un nuovo router usando il modulo 'express.Router()'
 const router = express.Router();
-
-// Importa il modulo 'database' dalla cartella 'config/helpers'
 const { database } = require('../config/helpers');
 
-/* GET ALL PRODUCTS */
+/**
+ * @description Gestisce la richiesta GET alla radice del percorso definito da questo router.
+ * @route GET /
+ * @param {Object} req - Oggetto della richiesta HTTP.
+ * @param {Object} res - Oggetto della risposta HTTP.
+ * @returns {Object} - Oggetto JSON contenente i prodotti trovati o un messaggio di errore.
+ */
 router.get('/', function(req, res) {
-  // Ottieni il valore della pagina dalla query della richiesta
-  // Imposta il valore predefinito come 1 se la query non è definita o è 0
-  let page = (req.query.page === undefined && req.query.page !== 0) ? req.query.page : 1;
-
-  // Ottieni il limite dalla query della richiesta
-  // Imposta il valore predefinito come 10 se la query non è definita o è 0
-  const limit = (req.query.limit === undefined && req.query.limit !== 0) ? req.query.limit : 10;
-
+  // Verifica se il parametro 'page' è definito e diverso da 0, altrimenti lo imposta a 1
+  let page = (req.query.page != undefined && req.query.page != 0 ) ? req.query.page : 1;
+  // Verifica se il parametro 'limit' è definito e diverso da 0, altrimenti lo imposta a 10
+  const limit = (req.query.limit != undefined && req.query.limit != 0 ) ? req.query.limit : 10;
   let startValue;
   let endValue;
 
-  if (page > 0) {
-    // Calcola i valori di startValue e endValue in base alla pagina e al limite
+  // Calcola i valori di inizio e fine in base al numero di pagina e al limite
+  if(page > 0){
     startValue = (page * limit) - limit;
     endValue = page * limit;
   } else {
-    // Se la pagina è inferiore o uguale a 0, imposta i valori predefiniti
+    // Se il numero di pagina è minore o uguale a 0, imposta i valori di default
     startValue = 0;
     endValue = 10;
   }
 
-  // Esegue una query sul database per ottenere i prodotti con determinati campi e condizioni
+  // Esegue la query al database per ottenere i prodotti con join sulla tabella delle categorie
   database.table('products as p')
-  .join([
-    {
-      table: 'categories as c',
+    .join([{
+      table:'categories as c',
       on: 'c.id = p.cat_id'
-    }
-  ])
-  .withFields([
-    'c.title as category',  // Seleziona il campo 'title' dalla tabella 'categories' con alias 'category'
-    'p.title as name',  // Seleziona il campo 'title' dalla tabella 'products' con alias 'name'
-    'p.price',  // Seleziona il campo 'price' dalla tabella 'products'
-    'p.quantity',  // Seleziona il campo 'quantity' dalla tabella 'products'
-    'p.image',  // Seleziona il campo 'image' dalla tabella 'products'
-    'p.id'  // Seleziona il campo 'id' dalla tabella 'products'
-  ])
-  .slice(startValue, endValue)  // Esegue una suddivisione dei risultati per ottenere solo un intervallo specifico
-  .sort({ id: -1 })  // Ordina i risultati in base all'ID in ordine decrescente (-1 indica l'ordine decrescente)
-  .getAll()  // Esegue la query e restituisce tutti i risultati
-  .then(prods => {
-    if (prods.length > 0) {
-      // Se ci sono prodotti trovati, invia una risposta con i dettagli dei prodotti
-      res.status(200).json({
-        count: prods.length,  // Numero totale di prodotti trovati
-        products: prods  // Array dei prodotti trovati
-      });
-    } else {
-      // Se non ci sono prodotti trovati, invia un messaggio di avviso
-      res.json({ message: 'No products found' });
-    }
-  })
-  .catch(err => console.log(err));  // Gestisce gli eventuali errori durante l'esecuzione della query
+    }])
+    .withFields([
+      'c.title as category', // Alias del campo "title" della tabella "categories" come "category"
+      'p.title as name', // Alias del campo "title" della tabella "products" come "name"
+      'p.price', // Campo "price" della tabella "products"
+      'p.quantity', // Campo "quantity" della tabella "products"
+      'p.image', // Campo "image" della tabella "products"
+      'p.id' // Campo "id" della tabella "products"
+    ])
+    .slice(startValue, endValue) // Limita i risultati in base ai valori di inizio e fine
+    .sort({ id: -1 }) // Ordina i risultati in ordine decrescente in base all'id
+    .getAll() // Ottiene tutti i risultati della query
+    .then(prods => {
+      if (prods.length > 0) {
+        // Se ci sono prodotti, restituisci una risposta JSON con i prodotti trovati
+        res.status(200).json({
+          count: prods.length, // Numero totale di prodotti restituiti
+          products: prods // Array dei prodotti
+        });
+      } else {
+        // Se non ci sono prodotti, restituisci un messaggio JSON
+        res.json({ message: 'Nessun prodotto trovato' });
+      }
+    })
+    .catch(err => console.log(err)); // Gestione degli errori durante l'esecuzione della query
 });
 
-// Esporta il router per essere utilizzato in altre parti dell'applicazione
+
+
+/**
+ * @description Ottiene un singolo prodotto.
+ * @route GET /:prodId
+ * @param {Object} req - Oggetto della richiesta HTTP.
+ * @param {Object} res - Oggetto della risposta HTTP.
+ * @returns {Object} - Oggetto JSON contenente il prodotto o un messaggio di errore.
+ */
+router.get('/:prodId', (req, res) => {
+  // Ottieni l'ID del prodotto dalla richiesta
+  let productId = req.params.prodId;
+  console.log(productId);
+
+  // Esegue la query al database per ottenere i prodotti con join sulla tabella delle categorie
+  database.table('products as p')
+    .join([{
+      table:'categories as c',
+      on: 'c.id = p.cat_id'
+    }])
+    .withFields([
+      'c.title as category', // Alias del campo "title" della tabella "categories" come "category"
+      'p.title as name', // Alias del campo "title" della tabella "products" come "name"
+      'p.price', // Campo "price" della tabella "products"
+      'p.quantity', // Campo "quantity" della tabella "products"
+      'p.image', // Campo "image" della tabella "products"
+      'p.images',
+      'p.id' // Campo "id" della tabella "products"
+    ])
+    .filter({'p.id': productId})
+    .get() // Ottiene tutti i risultati della query
+    .then(prod => {
+      if (prod) {
+        // Se ci sono prodotti, restituisci una risposta JSON con i prodotti trovati
+        res.status(200).json(prod);
+      } else {
+        // Se non ci sono prodotti, restituisci un messaggio JSON
+        res.json({ message: 'Nessun prodotto trovato con l\'ID del prodotto ' + productId });
+      }
+    })
+    .catch(err => console.log(err)); // Gestione degli errori durante l'esecuzione della query
+});
+
+
 module.exports = router;
