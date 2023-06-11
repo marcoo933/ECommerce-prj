294b2b25ebed5760015662dea606b06397b50913
MP93 <80913365+marcoo933@users.noreply.github.com>
2023-06-11 11:35:07 +0200

    created api orders from id

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -49,4 +49,55 @@ router.get("/", (req, res) => {
 		.catch((err) => console.log(err));
 });
 
+/**
+ * Rotta per ottenere un ordine specifico con i dettagli associati.
+ * @param {Object} req - Oggetto di richiesta HTTP.
+ * @param {Object} res - Oggetto di risposta HTTP.
+ */
+router.get('/:id', (req,res) => {
+  // Ottiene l'ID dell'ordine dalla richiesta dei parametri
+  const orderId = req.params.id;
+
+  // Esegue una query al database per ottenere l'ordine specifico con i dettagli associati
+  database
+  .table("orders_details as od")
+  .join([
+    {
+      table: "orders as o",
+      on: "o.id = od.order_id",
+    },
+    {
+      table: "products as p",
+      on: "p.id = od.product_id",
+    },
+    {
+      table: "users as u",
+      on: "u.id = o.user_id",
+    },
+  ])
+  // Seleziona i campi desiderati per la risposta
+  .withFields([
+    "o.id",
+    "p.title as name",
+    "p.description",
+    "p.price",
+    "u.username",
+  ])
+  // Filtra gli ordini per l'ID specificato
+  .filter({'o.id': orderId})
+  // Esegue la query e ottiene l'ordine specifico con i dettagli associati
+  .getAll()
+  .then((orders) => {
+    if (orders.length > 0) {
+      // Se è presente l'ordine, invia una risposta con lo status 200 e l'array dell'ordine
+      res.status(200).json(orders);
+    } else {
+      // Se l'ordine non è presente, invia una risposta con un messaggio di nessun ordine trovato
+      res.json({ message: "Nessun ordine trovato with orderId " + orderId });
+    }
+  })
+  .catch((err) => console.log(err));
+});
+
+
 module.exports = router;
